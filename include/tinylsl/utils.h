2079502fc004f5b31ab3b750b22a3f88f373f0c0
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2025-09-03 19:02:58 +0200

    added missing define MIN

--- include/tinylsl/utils.h
+++ include/tinylsl/utils.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 #define EMIT_DEFAULT(callbacks, name, default, ...) (((callbacks)->name) \
     ? ((callbacks)->name)((callbacks)->ctx, ##__VA_ARGS__) \
     : (default))
